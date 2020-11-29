3fa0b5933f0f27b14ab01745f3f7efbadaa753e7
pingfury <pingfury@outlook.com>
2020-11-29 10:24:08 +0000

    add migrate

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
-from app import create_app, db
 import os
+from flask_migrate import Migrate
+from app import create_app, db
+from app.models import Item, Group
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, Item=Item, Group=Group)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
